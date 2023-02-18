f5054964e771b0826b3a473dede4900632b2bb1b
kim3722 <kim3722@purdue.edu>
2023-02-18 03:06:46 -0500

    basic resampling

---
+++ resampling.py
@@ -0,0 +1,20 @@
+import random
+import numpy as np
+
+def resampling(Xt, Wt):
+    X_sm = np.array()
+    N = len(Wt)
+    positions = (radom(N) + range(N)) / N
+
+    indexes = np.zeros(N, 'i')
+    cumulative_sum = np.cumsum(Wt)
+    i , j = 0, 0
+    while i < N:
+        if positions[i] < cumulative_sum[j]:
+            indexes[i] = j
+            i += 1
+        else:
+            j += 1
+        
+    return Xt[indexes]
+
